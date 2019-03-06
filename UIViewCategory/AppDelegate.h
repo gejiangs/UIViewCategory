d6f17e80ca7dd40f55a0e8115560072c559dd25c
gejiangs <664553782@qq.com>
2019-03-06 10:59:40 +0800

    Initial Commit

---
+++ UIViewCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewCategory
+//
+//  Created by gejiangs on 2019/3/6.
+//  Copyright © 2019 gejiangs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewCategory
+//
+//  Created by gejiangs on 2019/3/6.
+//  Copyright © 2019 gejiangs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
